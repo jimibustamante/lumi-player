bc7f98b0d4e09940e2f4c19246a7f98782a5fa1d
Jaime Bustamante <jimibustamante@gmail.com>
2020-11-21 21:39:03 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const getVideos = async () => {
+    const resp = await window.LumiApi.getFolderVideos();
+    if (resp) {
+      setVideos(resp);
+    }
+  }
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  useEffect(() => {
+    console.log({videos});
+  }, [videos])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lumi Player</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import LumiApi from './lib/api';
+window.LumiApi = new LumiApi();
 ReactDOM.render(
   <React.StrictMode>
     <App />
